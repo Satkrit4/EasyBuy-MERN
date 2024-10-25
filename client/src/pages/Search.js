5c1b1cb9f5b3a8e2911cbb588ff3645942f884fa
Satkrit4 <114677990+Satkrit4@users.noreply.github.com>
2024-10-25 23:39:55 +0530

    Resolved env error in frontend

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -21,7 +21,7 @@ const Search = () => {
             {values?.results.map((p) => (
               <div className="card m-2" style={{ width: "18rem" }}>
                 <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
+                  src={`${process.env.REACT_APP_BACKEND_URL}/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
